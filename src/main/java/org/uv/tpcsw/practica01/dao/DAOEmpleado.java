1fe193d99c79203a6c39b67bb291a877f5412af9
robertrooch <robertorooch2004@gmail.com>
2024-09-01 19:30:10 -0600

    patron DAO

---
+++ src/main/java/org/uv/tpcsw/practica01/dao/DAOEmpleado.java
@@ -0,0 +1,22 @@
+package org.uv.tpcsw.practica01.dao;
+
+import java.util.List;
+
+//CRUD
+public class DAOEmpleado {
+    public boolean guardar(EmpleadoPojo pojo){
+        return true;
+    }
+    public boolean eliminar(EmpleadoPojo pojo){
+        return true;
+    }
+    public boolean modificar(EmpleadoPojo pojo){
+        return true;
+    }
+    public List<EmpleadoPojo> buscarAll(){
+        return null;
+    }
+    public EmpleadoPojo buscarById(EmpleadoPojo pojo){
+        return null;
+    }
+}
---
+++ src/main/java/org/uv/tpcsw/practica01/dao/DAOEmpleadoConcreto.java
@@ -0,0 +1,32 @@
+package org.uv.tpcsw.practica01.dao;
+
+import java.util.List;
+
+public class DAOEmpleadoConcreto implements IDAOGeneral<EmpleadoPojo, String> {
+
+    @Override
+    public boolean save(EmpleadoPojo pojo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(String pojo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(EmpleadoPojo pojo, String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<EmpleadoPojo> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public EmpleadoPojo findById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
